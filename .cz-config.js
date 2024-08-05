84f96c31b0634fb2f9abae3f798cf46d1c2136d9
Mike <mike.costa@bs2tecnologia.com.br>
2024-08-05 19:08:28 -0300

    chore(configs): #1000 adicionando coniguracao de commitlint

--- .cz-config.js
+++ .cz-config.js
@@ -38,6 +38,7 @@ module.exports = {
   isTicketNumberRequired: false,
   ticketNumberPrefix: "#",
   ticketNumberRegExp: "\\d{1,5}",
+  appendBranchNameToCommitMessage: true,
   // override the messages, defaults are as follows
   messages: {
     type: "Selecione o tipo de mudança do seu commit:",
@@ -46,10 +47,11 @@ module.exports = {
     customScope: 'Denote the SCOPE of this change:',
     subject: 'Escreva uma descrição CURTA, IMPERATIVA sobre a mudança:\n',
     body: 'Forneça uma descrição MAIS LONGA da alteração (opcional). Use "|" para quebrar nova linha:\n',
-    breaking: 'List any BREAKING CHANGES (optional):\n',
+    ticketNumber: 'Número do card do BOARD (somente os números):',
+    breaking: 'Liste as BREAKING CHANGES (optional):\n',
     footer: 'Liste quaisquer demandas do AZURE DEVOPS para esta mudança (opcional) Ex.: #1100, #1200:\n',
     confirmCommit: 'Tem certeza de que deseja prosseguir com o commit acima?'
   },
+  skipQuestions: ['footer'],
   breaklineChar: '|', // It is supported for fields body and footer.
-  footerPrefix: 'AZURE DEVOPS: '
 };
